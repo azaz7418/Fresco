878def38442be2e068160ed87ec400958de3d933
azaz7418 <wazaz7418@gmail.com>
2025-03-16 20:06:17 +0600

    menu item-half complete

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -23,38 +23,73 @@ const Menu = () => {
       </div>
 
       {/* part 02 */}
-
-      <div className=" bg-gray-100 px-20 py-10 flex items-center justify-center ">
-        <div className=" flex items-center justify-center gap-5 w-[75%]">
-          {/* Left Image Section */}
-          <div className=" w-[45%] ">
-            <img src={menuData.image} alt="Pasta" className="rounded-xl" />
-          </div>
-          {/* Right Menu Section */}
-          <div className=" w-[55%] p-6 h-full">
-            <div className=" space-y-5 py-5">
-              <h2 className="text-4xl text-black font-bold font-serif mb-4">Pasta</h2>
-              <div className=" w-[25%]">
-                <img src={menuData.underLine} alt="" />
+      {menuData.map((itm, index) => {
+        return index % 2 == 0 ? (
+          <div key={index} className=" bg-gray-100 px-20 py-10 flex items-center justify-center ">
+            <div className=" flex items-center justify-center gap-5 w-[75%]">
+              {/* Left Image Section */}
+              <div className=" w-[45%] ">
+                <img src={itm.image} alt="Pasta" className="rounded-xl" />
+              </div>
+              {/* Right Menu Section */}
+              <div className=" w-[55%] p-6 h-full">
+                <div className=" space-y-5 py-5">
+                  <h2 className="text-4xl text-black font-bold font-serif mb-4">{itm.category}</h2>
+                  <div className=" w-[25%]">
+                    <img src={itm.underLine} alt="" />
+                  </div>
+                </div>
+                <div className="space-y-7">
+                  {/* Menu Items */}
+                  {itm.items?.map((it, index) => {
+                    return (
+                      <div key={index} className="flex justify-between items-center">
+                        <div>
+                          <h3 className="text-xl mb-3 text-black font-semibold">{it.title}</h3>
+                          <p className="text-gray-500 text-sm">{it.description}</p>
+                        </div>
+                        <span className="text-xl text-black font-bold">{it.price}</span>
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             </div>
-            <div className="space-y-7">
-              {/* Menu Items */}
-              {menuData.items?.map((it, index) => {
-                return (
-                  <div key={index} className="flex justify-between items-center">
-                    <div>
-                      <h3 className="text-xl mb-3 text-black font-semibold">{it.title}</h3>
-                      <p className="text-gray-500 text-sm">{it.description}</p>
-                    </div>
-                    <span className="text-xl text-black font-bold">{it.price}</span>
+          </div>
+        ) : (
+          <div key={index} className=" bg-orange-50 px-20 py-10 flex items-center justify-center ">
+            <div className=" flex items-center justify-center gap-5 w-[75%]">
+              {/* Left Menu Section */}
+              <div className=" w-[55%] p-6 h-full">
+                <div className=" space-y-5 py-5">
+                  <h2 className="text-4xl text-black font-bold font-serif mb-4">{itm.category}</h2>
+                  <div className=" w-[25%]">
+                    <img src={itm.underLine} alt="" />
                   </div>
-                );
-              })}
+                </div>
+                <div className="space-y-7">
+                  {/* Menu Items */}
+                  {itm.items?.map((it, index) => {
+                    return (
+                      <div key={index} className="flex justify-between items-center">
+                        <div>
+                          <h3 className="text-xl mb-3 text-black font-semibold">{it.title}</h3>
+                          <p className="text-gray-500 text-sm">{it.description}</p>
+                        </div>
+                        <span className="text-xl text-black font-bold">{it.price}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              {/* Right Image Section */}
+              <div className=" w-[45%] ">
+                <img src={itm.image} alt="Pasta" className="rounded-xl" />
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 };
--- src/pages/menu/MenuItem.jsx
+++ src/pages/menu/MenuItem.jsx
@@ -1,27 +1,85 @@
-export const menuData = {
-  category: "Pasta",
-  image: "https://websitedemos.net/italian-restaurant-02/wp-content/uploads/sites/283/2018/10/pic39-free-img.jpg",
-  underLine: "https://websitedemos.net/italian-restaurant-02/wp-content/uploads/sites/283/2018/09/divider-free-img.png",
-  items: [
-    {
-      title: "Whole Grain Pasta",
-      description: "Roasted eggplant spread, marinated tomatoes with garlic & fresh basil",
-      price: "$25.00",
-    },
-    {
-      title: "Tortellini Gorgonzola",
-      description: "Roasted eggplant spread, marinated tomatoes with garlic & fresh basil",
-      price: "$18.50",
-    },
-    {
-      title: "Rigatoni Zuccati",
-      description: "Roasted eggplant spread, marinated tomatoes with garlic & fresh basil",
-      price: "$22.50",
-    },
-    {
-      title: "Spaghetti Marinara",
-      description: "Roasted eggplant spread, marinated tomatoes with garlic & fresh basil",
-      price: "$21.50",
-    },
-  ],
-};
+export const menuData = [
+  {
+    category: "Pasta",
+    image: "https://websitedemos.net/italian-restaurant-02/wp-content/uploads/sites/283/2018/10/pic39-free-img.jpg",
+    underLine:
+      "https://websitedemos.net/italian-restaurant-02/wp-content/uploads/sites/283/2018/09/divider-free-img.png",
+    items: [
+      {
+        title: "Whole Grain Pasta",
+        description: "Roasted eggplant spread, marinated tomatoes with garlic & fresh basil",
+        price: "$25.00",
+      },
+      {
+        title: "Tortellini Gorgonzola",
+        description: "Roasted eggplant spread, marinated tomatoes with garlic & fresh basil",
+        price: "$18.50",
+      },
+      {
+        title: "Rigatoni Zuccati",
+        description: "Roasted eggplant spread, marinated tomatoes with garlic & fresh basil",
+        price: "$22.50",
+      },
+      {
+        title: "Spaghetti Marinara",
+        description: "Roasted eggplant spread, marinated tomatoes with garlic & fresh basil",
+        price: "$21.50",
+      },
+    ],
+  },
+  {
+    category: "Pizza",
+    image: "https://websitedemos.net/italian-restaurant-02/wp-content/uploads/sites/283/2018/10/pic62-free-img.jpg",
+    underLine:
+      "https://websitedemos.net/italian-restaurant-02/wp-content/uploads/sites/283/2018/09/divider-free-img.png",
+    items: [
+      {
+        title: "BBQ Special Pizza",
+        description: "Roasted eggplant spread, marinated tomatoes with garlic & fresh basil",
+        price: "$25.00",
+      },
+      {
+        title: "Spe. Cheesy Cheese",
+        description: "Roasted eggplant spread, marinated tomatoes with garlic & fresh basil",
+        price: "$18.50",
+      },
+      {
+        title: "Mushroom Chilly Pizza",
+        description: "Roasted eggplant spread, marinated tomatoes with garlic & fresh basil",
+        price: "$22.50",
+      },
+      {
+        title: "Chicken BBQ Special",
+        description: "Roasted eggplant spread, marinated tomatoes with garlic & fresh basil",
+        price: "$21.50",
+      },
+    ],
+  },
+  {
+    category: "Platter",
+    image: "https://websitedemos.net/italian-restaurant-02/wp-content/uploads/sites/283/2019/12/platter-img.jpg",
+    underLine:"https://websitedemos.net/italian-restaurant-02/wp-content/uploads/sites/283/2018/09/divider-free-img.png",
+    items: [
+      {
+        title: "Sautéed Spinach",
+        description: "Roasted eggplant spread, marinated tomatoes with garlic & fresh basil",
+        price: "$25.00",
+      },
+      {
+        title: "Roasted Butternut Squash",
+        description: "Roasted eggplant spread, marinated tomatoes with garlic & fresh basil",
+        price: "$18.50",
+      },
+      {
+        title: "Crumbled Sausage",
+        description: "Roasted eggplant spread, marinated tomatoes with garlic & fresh basil",
+        price: "$22.50",
+      },
+      {
+        title: "Vegetable Sauté",
+        description: "Roasted eggplant spread, marinated tomatoes with garlic & fresh basil",
+        price: "$21.50",
+      },
+    ],
+  },
+];
